fc4945ae2d1060adbb529dc44ff7563a88c432f3
mithunaorko <97835220+mithunaorko@users.noreply.github.com>
2023-06-21 01:55:49 +0600

    [DONE]Handle Same Year Same Semester Validation

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,6 @@
+import status from 'http-status';
 import { Schema, model } from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import {
   academicSemesterCodes,
   academicSemesterMonths,
@@ -42,8 +44,26 @@ export const academicSemesterSchema = new Schema<IAcademicSemester>(
   }
 );
 
+// using mongoose pre hook for when created data before validate pre hook is data is already exist or not we are using for same season(Autumn, Summer, Fall) is exist or not
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+
+  if (isExist) {
+    throw new ApiError(
+      status.CONFLICT,
+      'Academic semester season is already exist for this year!'
+    );
+  }
+  next();
+});
+
 // create a model for academic semester
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
   academicSemesterSchema
 );
+
+
